98ad3d543d81dd96fe686d51e56bfee0cc5c4b98
Yanay-Sella <Yanaysella@gmail.com>
2023-07-05 16:30:42 +0300

    validation when posting and getting users

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,10 +1,79 @@
 import React from "react";
+import Logo from "../components/Logo.jsx";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faMugSaucer,
+  faPencil,
+  faTrashCan,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Users = () => {
+  const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+  const [usersArr, setUsersArr] = useState([]);
+
+  useEffect(() => {
+    const getAllUsers = async () => {
+      try {
+        const response = await axios.get(`${serverUrl}/user`);
+        console.log(response);
+        setUsersArr(response.data); //array from db
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllUsers();
+  }, []);
+
+  useEffect(() => {
+    console.log(usersArr);
+  }, [usersArr]);
+
   return (
-    <>
-      <h1>hello Users</h1>
-    </>
+    <div className="flex flex-col items-center gap-6 w-96">
+      <Logo />
+      {usersArr && (
+        <div className="flex flex-col gap-4 items-center width-full text-white fonty">
+          {usersArr.map((element) => {
+            const { fnm, lnm, email, phone, comment } = element;
+            return (
+              <User
+                fnm={fnm}
+                lnm={lnm}
+                email={email}
+                phone={phone}
+                comment={comment}
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const User = ({ fnm, lnm, email, phone, comment }) => {
+  return (
+    <div className="relative border-2 rounded-md w-96 p-3 transition-all hover:scale-105">
+      <div>
+        <p className="text-3xl">{`${fnm} ${lnm}`}</p>
+        <p>{`Email: ${email}`}</p>
+        <p>{`Phone number: ${phone}`}</p>
+        <p>{`Comment: ${comment}`}</p>
+      </div>
+      <div className="absolute right-2 bottom-2 flex gap-2">
+        <div className="hover:cursor-pointer text-white hover:bg-gray-900 rounded-full flex p-2">
+          <FontAwesomeIcon icon={faPencil} />
+        </div>
+        <div className="hover:cursor-pointer text-white hover:bg-gray-900 rounded-full flex p-2">
+          <FontAwesomeIcon icon={faTrashCan} />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- server/user-controller.js
+++ server/user-controller.js
@@ -2,22 +2,40 @@ const pool = require("./database/db.js");
 
 const getAllUsers = async (req, res) => {
   try {
-    return res.status(200).json({ message: `gg` });
+    const allUsers = await pool.query("SELECT * FROM users");
+    return res.status(200).json(allUsers.rows);
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ message: "server error" });
   }
 };
 
 const handleSignUp = async (req, res) => {
   try {
-    console.log(req.body);
     const { fnm, lnm, email, phone, comment } = req.body;
+
+    //validation
+    const namePattern = /^[a-z]{1,10}$/i; //first and last
+    const emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const phonePattern = /^0\d{9}$/;
+
+    if (!namePattern.test(fnm) || !namePattern.test(lnm))
+      return res.status(400).json({ message: "Name not valid" });
+    if (!emailPattern.test(email))
+      return res.status(400).json({ message: "email not valid" });
+    if (!phonePattern.test(phone))
+      return res.status(400).json({ message: "phone not valid" });
+    if (typeof comment !== "string") {
+      return res.status(400).json({ message: "comment not valid" });
+    }
+    //end validation
+
     const newUser = await pool.query(
-      "INSERT INTO users (fnm, lnm, email, phone, comment) values ($1, $2, $3, $4, $5)",
+      "INSERT INTO users (fnm, lnm, email, phone, comment) values ($1, $2, $3, $4, $5) RETURNING *",
       [fnm, lnm, email, phone, comment]
     );
-    console.log(`user ${fnm} added succefully`);
-    return res.status(200).json({ message: `user ${fnm} added succefully` });
+    console.log(newUser);
+    return res.status(200).json(newUser.rows[0]);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "server error" });
